ac3565345c6d289556885dfd94eb490d4cc37e8c
atulj4723 <atulj9537@gmail.com>
2025-08-22 12:42:22 +0530

    deploy test 3

--- frontend/src/app/(pages)/userdata/page.jsx
+++ frontend/src/app/(pages)/userdata/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-    return <div></div>;
+    return <div>userdata</div>;
 };
 
 export default page;
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-    return <div className=" w-full h-[100vh]"></div>;
+    return <div className=" w-full h-[100vh]">jhvkjhvkh</div>;
 };
 
 export default page;
