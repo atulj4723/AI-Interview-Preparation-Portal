d02333ec3a84c32c0778f77e16d09745cde5b1b9
atulj4723 <atulj9537@gmail.com>
2025-08-22 17:00:03 +0530

    backend deploy test 2

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.auth import auth_bp
 from flask_cors import CORS
+port = 3000 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(auth_bp)
@@ -8,4 +9,5 @@ app.register_blueprint(auth_bp)
 def hello_world():
     return 'Hello, World!'
 if __name__ == '__main__':
-    app.run(debug="true")
+    # app.run(debug="true")
+    app.run(port=3000)
